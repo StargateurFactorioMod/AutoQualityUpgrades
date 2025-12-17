d53163c71cb49226d24e56a0bd8b403eaf24dfe4
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2025-12-17 23:34:24 +0100

    0.2.0

--- control.lua
+++ control.lua
@@ -1,3 +1,5 @@
+local util = require("util")
+
 local aqu = {}
 
 aqu.debug = false
@@ -7,7 +9,7 @@ aqu.debug = false
 ---@field entities LuaEntity[]
 ---@field current_index integer
 --- normal not included
----@field qualities string[]
+---@field qualities LuaQualityPrototype[]
 ---@field entities_by_run int
 
 ---@type Storage
@@ -39,6 +41,27 @@ function aqu.process_entity_to_be_upgraded(entity)
 end
 
 ---@param entity LuaEntity
+---@param networks LuaLogisticNetwork[]
+---@param prototype LuaEntityPrototype
+---@param allow_downgrade boolean
+function aqu.try_upgrade(entity, networks, prototype, allow_downgrade)
+  for i = #storage.qualities, 2, -1 do
+    local quality = storage.qualities[i]
+    if not allow_downgrade and quality.name == entity.quality.name then
+      return
+    end
+    for _, item in ipairs(prototype.items_to_place_this) do
+      for _, network in ipairs(networks) do
+        if network.can_satisfy_request({ name = item.name, quality = quality }, 1, true) then
+          entity.order_upgrade { target = { name = prototype.name, quality = quality }, force = entity.force }
+          return
+        end
+      end
+    end
+  end
+end
+
+---@param entity LuaEntity
 function aqu.process_entity(entity)
   if entity.to_be_deconstructed() then
     return
@@ -54,25 +77,23 @@ function aqu.process_entity(entity)
     return
   end
 
-  for i = #storage.qualities, 1, -1 do
-    local quality_name = storage.qualities[i]
-    if quality_name == entity.quality.name then
-      return
+  if entity.type == "entity-ghost" then
+    if entity.is_registered_for_construction() then
+      aqu.try_upgrade(entity, networks, entity.ghost_prototype--[[@as LuaEntityPrototype]], true)
     end
-    local quality = prototypes.quality[quality_name]
-    for _, item in ipairs(entity.prototype.items_to_place_this) do
-      for _, network in ipairs(networks) do
-        if network.can_satisfy_request({ name = item.name, quality = quality }, 1, true) then
-          entity.order_upgrade { target = { name = entity.name, quality = quality }, force = entity.force }
-          return
-        end
-      end
-    end
+  else
+    aqu.try_upgrade(entity, networks, entity.prototype, false)
   end
 end
 
 function aqu.init_entities_by_run()
   storage.entities_by_run = math.max(1, #storage.entities / settings.global["aqu-ticks-by-cycle"].value)
+end
+
+---@param entity LuaEntity
+---@return boolean
+function aqu.fully_upgraded(entity)
+  return false
 end
 
 ---@param tick NthTickEventData
@@ -81,7 +102,7 @@ function aqu.run(tick)
     if storage.current_index <= #storage.entities then
       local entity = storage.entities[storage.current_index]
 
-      if entity and entity.valid and entity.quality.next then
+      if entity and entity.valid and not aqu.fully_upgraded(entity) then
         aqu.process_entity(entity)
         storage.current_index = storage.current_index + 1
       else
@@ -98,8 +119,14 @@ end
 
 ---@param entity LuaEntity
 function aqu.add_entity(entity)
-  local items_to_place_this = entity.prototype.items_to_place_this
-  if items_to_place_this and #items_to_place_this > 0 and entity.quality.next then
+  local prototype
+  if entity.type == "entity-ghost" then
+    prototype = entity.ghost_prototype
+  else
+    prototype = entity.prototype
+  end
+  local items_to_place_this = prototype.items_to_place_this
+  if items_to_place_this and #items_to_place_this > 0 and not aqu.fully_upgraded(entity) then
     table.insert(storage.entities, entity)
   end
 end
@@ -117,19 +144,47 @@ function aqu.info(command)
   print(string.format("Entities count: %d, Current index: %d", #storage.entities, storage.current_index))
   print(string.format("Current rate is %d entites every %d ticks", storage.entities_by_run, aqu.tick()))
 
-  ---@type table<string, int>
-  local types_count = {}
+  ---@type table<string, table<string, int>>
+  local entities_count_per_quality = {}
+  local entities_ghost_count_per_quality = {}
   for _, entity in ipairs(storage.entities) do
     if entity.valid then
-      if not types_count[entity.type] then
-        types_count[entity.type] = 1
+      if entity.type == "entity-ghost" then
+        local name = entity.ghost_prototype.name
+        if not entities_ghost_count_per_quality[entity.quality.name] then
+          entities_ghost_count_per_quality[entity.quality.name] = {}
+        end
+        if not entities_ghost_count_per_quality[entity.quality.name][name] then
+          entities_ghost_count_per_quality[entity.quality.name][name] = 1
+        else
+          util.increment(entities_ghost_count_per_quality[entity.quality.name], name)
+        end
       else
-        types_count[entity.type] = types_count[entity.type] + 1
+        if not entities_count_per_quality[entity.quality.name] then
+          entities_count_per_quality[entity.quality.name] = {}
+        end
+        if not entities_count_per_quality[entity.quality.name][entity.name] then
+          entities_count_per_quality[entity.quality.name][entity.name] = 1
+        else
+          util.increment(entities_count_per_quality[entity.quality.name], entity.name)
+        end
       end
     end
+
   end
-  for type, count in pairs(types_count) do
-    print(string.format("%s: %d", type, count))
+  for quality_name, entities_count in pairs(entities_count_per_quality) do
+    local line = string.format("[img=quality.%s]", quality_name)
+    for entity_name, count in pairs(entities_count) do
+      line = string.format("%s [img=entity.%s] %d", line, entity_name, count)
+    end
+    print(line)
+  end
+  for quality_name, entities_count in pairs(entities_ghost_count_per_quality) do
+    local line = string.format("[img=entity.entity-ghost][img=quality.%s]", quality_name)
+    for entity_name, count in pairs(entities_count) do
+      line = string.format("%s [img=entity.%s] %d", line, entity_name, count)
+    end
+    print(line)
   end
 end
 
@@ -139,6 +194,7 @@ function aqu.event_filters()
   local event_filters = {}
   for _, type in ipairs(filters) do
     table.insert(event_filters, { filter = "type", type = type })
+    table.insert(event_filters, { filter = "ghost_type", type = type })
   end
 
   return event_filters
@@ -147,7 +203,7 @@ end
 ---@return string[]
 function aqu.filters()
   local filters = {}
-  for type in settings.global["aqu-watch-types"].value --[[@as string]]:gmatch("([^,]+)") do
+  for type in settings.global["aqu-watch-types"].value--[[@as string]]:gmatch("([^,]+)") do
     type = type:match("^%s*(.-)%s*$")
     table.insert(filters, type)
   end
@@ -164,12 +220,16 @@ function aqu.init()
     for _, entity in ipairs(surface.find_entities_filtered({ type = filters })) do
       aqu.add_entity(entity)
     end
+
+    for _, entity in ipairs(surface.find_entities_filtered({ ghost_type = filters })) do
+      aqu.add_entity(entity)
+    end
   end
 
   storage.qualities = {}
   local quality = prototypes.quality["normal"]
-  while quality.next do
-    table.insert(storage.qualities, quality.next.name)
+  while quality do
+    table.insert(storage.qualities, quality)
     quality = quality.next
   end
 
